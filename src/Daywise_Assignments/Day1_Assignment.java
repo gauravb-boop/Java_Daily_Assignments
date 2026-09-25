6141e789ebdf203149f3ea7102e0a0c905c06e0a
gauravb-boop <gauravbhandari2309@gmail.com>
2026-09-25 17:17:43 +0530

    Day1_Assignment

--- src/Daywise_Assignments/Day1_Assignment.java
+++ src/Daywise_Assignments/Day1_Assignment.java
@@ -12,14 +12,14 @@ public class Day1_Assignment {
         int x=5;
         int y=x++;  // y = 5 ; x = 5 +1 = 6
         int z=++x;  // x = 6 +1 =7 ; z = 7
-        System.out.println(y);
-        System.out.println(z);
+        System.out.println(y); // 5
+        System.out.println(z); // 7
 
         //3.What is d?
         double d = 10 / 4;  // 10/4 = 2.0
         double e = 10.0 / 4;//  10 % 4 = 2.5
-        System.out.println(d);
-        System.out.println(e);
+        System.out.println(d);  // 2.0
+        System.out.println(e); // 2.5
 
         //4.Fix:
         //float price = 99.99;  have to put f at the last so compiler recognize vale as float
@@ -32,10 +32,10 @@ public class Day1_Assignment {
         //6.Write a program using ?: to print PASS/FAIL.
         int f = 90 ;
         double g = 90.01;
-        System.out.println(f > g ? "Pass" : "Fail");
-        System.out.println(f < g ? "Pass" : "Fail");
-        System.out.println(f == g ? "Pass" : "Fail");
-        System.out.println(f != g ? "Pass" : "Fail");
+        System.out.println(f > g ? "Pass" : "Fail");  // Fail
+        System.out.println(f < g ? "Pass" : "Fail");  // Pass
+        System.out.println(f == g ? "Pass" : "Fail");  // Fail
+        System.out.println(f != g ? "Pass" : "Fail");  // Pass
 
         //7.Build a salary calculator:
         int basic = 50000;
@@ -45,9 +45,9 @@ public class Day1_Assignment {
         float gross = basic + bonus;  // 50000 + 5000 = 55000
         float tax = (gross*taxper)/100;   // (55000 * 10)/100 = 5500
         float netsal = gross - tax ;   // 55000 - 5500 = 49500
-        System.out.println("Gross Salary = " + gross);
-        System.out.println("Total tax to pay = " + tax);
-        System.out.println("Net Salary = " + netsal);
+        System.out.println("Gross Salary = " + gross);  // 55000
+        System.out.println("Total tax to pay = " + tax);  // 5500
+        System.out.println("Net Salary = " + netsal);  //  49500
 
         //8. Mini Project - Student Result
 
